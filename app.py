3749356ef1ec2a5732dbb41b7671de562e87f373
beastinblack1995 <109196514+beastinblack1995@users.noreply.github.com>
2022-07-18 12:23:10 +0530

    Update app.py

--- app.py
+++ app.py
@@ -134,13 +134,13 @@ def upload_image():
                 y1, x2, y2, x1 = faceloc
                 y1, x2, y2, x1 = y1*4, x2*4, y2*4, x1*4
                 
-                cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 3)
-                cv2.rectangle(frame, (x1, y2-25), (x2, y2), (0, 255, 0), cv2.FILLED)
-                cv2.putText(frame, name, (x1+6, y2-6), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 255, 255), 2)
+                #cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 3)
+                #cv2.rectangle(frame, (x1, y2-25), (x2, y2), (0, 255, 0), cv2.FILLED)
+                #cv2.putText(frame, name, (x1+6, y2-6), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 255, 255), 2)
                 
-                #startX, startY, endX, endY = faceloc
-                #cv2.rectangle(frame, (startX, startY), (endX, endY), (0, 255, 0), 2)
-                #cv2.putText(frame, name, (endY+6, endX-6), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 255, 255), 2)
+                startX, startY, endX, endY = faceloc
+                cv2.rectangle(frame, (startX, startY), (endX, endY), (0, 255, 0), 2)
+                cv2.putText(frame, name, (endY+6, endX-6), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 255, 255), 2)
                 print(name)
                 #MarkAttendence(name)
                 
